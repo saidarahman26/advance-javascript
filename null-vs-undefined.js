e73e1467934763f8e200856009bc2c0e7efe088c
unknown <you@example.comgit config --global user.email you@example.com>
2021-11-20 12:03:54 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+
+let pakhi;
+
+function add(num1,num2){
+   console.log(num1+num2);
+   return 
+}
+
+function add(num1,num2){
+   console.log(num1,num2);
+}
+
+const premik = {name:"smart dude",phone:01234};
+console.log(premik.address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [3,5,8];
+console.log(ages[11]);
+
+//....NULL...//
---
+++ true-false.js
@@ -0,0 +1,17 @@
+// falsy:
+// false
+// 0
+// ""
+// NaN
+// undefined
+// null
+// truthy:
+// '0' "  " []
+let name = 12;
+if(name){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
