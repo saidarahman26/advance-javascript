cd65d091ff1ed8c73bd869bc4caffe3a773e36b5
unknown <you@example.comgit config --global user.email you@example.com>
2021-11-26 23:57:04 +0600

    advance-java

---
+++ array-split.js
@@ -0,0 +1,12 @@
+const nums = [1,2,3,4,5,6,7,8]
+//const part = nums.slice(2,5);
+//const removed = nums.splice(2,3,77,88,99);
+const together = nums.join(",")
+console.log(together);
+console.log(nums);
+const list = [11,22,33,44,55,66,77,88,99];
+const part = list.slice(2,6);
+const remove=list.splice(2,3,22,22,22);
+const connect = list.join("lalala");
+console.log(connect);
+console.log(list);
